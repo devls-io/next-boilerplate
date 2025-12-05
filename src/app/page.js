ca22ff27ba3290479496e9797d34572ba00474bd
Leonardo Sudré <165829938+devls-io@users.noreply.github.com>
2025-12-05 16:32:16 -0300

    feat: initial release of next-boilerplate v1.0
    - Configured Next.js 16 (App Router) + React 19
    
    - Setup Tailwind CSS v4
    
    - Integrated ESLint 9 (Flat Config) + Prettier
    
    - Added Jest + React Testing Library environment
    
    - Added project documentation (README)

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  const teste = "bagunça";
+  return (
+    <div>
+      <h1>Lalala</h1>
+
+      <p>lorem lorem lorem bla bla</p>
+      <h2>Identação pra que ? </h2>
+    </div>
+  );
+}
